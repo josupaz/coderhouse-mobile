312efc6659b0c19d88d7402a222e27374d60a911
unknown <jpaz@xytech.com.ar>
2022-04-24 22:24:33 -0300

    fix mobile-1

---
+++ Styles/Global.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native"
+
+export const colors = {
+    darkBlue: '#006d77',
+    light: '#edf6f9',
+    brown: '#e29578',
+    lightBrown: '#ffddd2'
+}
+
+export default StyleSheet.create({
+    container: {
+        paddingVertical: 25,
+        paddingHorizontal: 10,
+        backgroundColor: colors.light
+    },
+    
+})
